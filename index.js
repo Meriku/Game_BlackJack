1c1eac3140183d7c8749b3f5a51632b8cd53520f
Meriku <m.mirik.v@gmail.com>
2022-10-21 15:45:38 +0300

    Practice with arrays and loops. Tiny refactoring.

--- index.js
+++ index.js
@@ -1,68 +1,84 @@
 let startEl = document.getElementById("start-el")
 let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let cardsDealerEl = document.getElementById("cardsDealer-el")
+let resultEl = document.getElementById("result-el")
 let startButEl = document.getElementById("startBut-el")
 let endButEl = document.getElementById("endBut-el")
-let cardsEl = document.getElementById("cards-el")
-let cardsDealer = document.getElementById("cardsDealer-el")
-let resultEl = document.getElementById("result-el")
-let gameEnded = true
-let sum = 0
-let dealerSum = 0
+let nextCardButEl = document.getElementById("nextCardBut-el")
+
+let cards = []
+let dealerCards = []
 
 function Start() {
-    
-    if (gameEnded) {
-        gameEnded = false
-        Clear()
-        startEl.hidden = true
-        endButEl.hidden = false
-        startButEl.innerText = "Add card"
-    }
-    
-    let card = GetCard()
-    UpdateSum(card)
-    UpdateCards(card) 
+    cards = []
+    dealerCards = []
 
-    if (sum > 21) {
+    UpdateCards()
+    UpdateSum()
+
+    cardsDealerEl.textContent = ""
+    Hide([startEl, startButEl, cardsDealerEl])
+    Show([sumEl, cardsEl, nextCardButEl, endButEl])
+}
+
+function NextCard() {
+    let newCard = GetCard()
+    cards.push(newCard)
+    UpdateCards()
+    UpdateSum()
+    CheckForLose()
+}
+
+function CheckForLose() {
+    if (GetCardsSum() > 21) {
         End()
     }
-    
 }
 
 function End() {
-    gameEnded = true
 
-    if (sum > 21) {
-        resultEl.innerText = "Result:  You lose"
-        startButEl.innerText = "Start again"
-        startEl.hidden = false
-        endButEl.hidden = true
+    let result = GetCardsSum()
+    let dealerResult = GetDealerCards(result)
+
+    if (result <= 21 && dealerResult === result) {
+        Draw()
+    }
+    else if (result <= 21 && (result > dealerResult || dealerResult > 21)) {
+        Won()
     }
     else {
-        if (GetDealerCards() > 21) {
-            resultEl.innerText = "Result:  Victory"
-            startButEl.innerText = "Start again"
-            startEl.hidden = false
-            endButEl.hidden = true
-        }
-        else {
-            resultEl.innerText = "Result:  You lose"
-            startButEl.innerText = "Start again"
-            startEl.hidden = false
-            endButEl.hidden = true
-        }
+        Lose()
     }
- 
+    EndGame()
 }
 
-function GetDealerCards() {
-    while (dealerSum < sum && dealerSum < 21) {
+// important to have 3 different functions to implement Global Score later
+function Lose() {
+    startEl.textContent = "You lose. Want to start a new game?"
+}
+function Won() {
+    startEl.textContent = "You won. Want to start a new game?"
+}
+function Draw() {
+    startEl.textContent = "Draw. Want to start a new game?"
+}
+
+function EndGame() {
+    Show([startEl, startButEl])
+    Hide([nextCardButEl, endButEl])
+}
+
+function GetDealerCards(playerSum) {
+    let dealerSum = 0
+    while (dealerSum <= playerSum && dealerSum < 21) {
         let card = GetCard()
+        dealerCards.push(card)
         dealerSum += card
-        cardsDealer.innerText += " " + card 
+        cardsDealerEl.innerText += " " + card 
     }
-    cardsDealer.hidden = false
-    cardsDealer.innerText += " Sum: " + dealerSum
+    cardsDealerEl.hidden = false
+    cardsDealerEl.innerText += " Sum: " + dealerSum
     return dealerSum
 }
 
@@ -70,20 +86,42 @@ function GetCard() {
     return Math.floor(Math.random() * 10 + 1)
 }
 
-function UpdateSum(card) {
-    sum += card
-    sumEl.innerText = "Sum: " + sum
+function GetCardsSum() {
+    let sum = 0
+
+    for (let i = 0; i < cards.length; i++) {
+        sum += cards[i]
+    }
+    return sum
 }
 
-function UpdateCards(card) {
-    cardsEl.innerText += " " + card
+function UpdateSum() {
+    let sum = GetCardsSum()
+    sumEl.textContent = "Sum: "
+    if (sum > 0) {
+        sumEl.textContent += sum
+    }
 }
 
-function Clear() {
-    sum = 0
-    dealerSum = 0
-    sumEl.innerText = "Sum: "
-    cardsEl.innerText = "Cards: "
-    cardsDealer.hidden = true
-    cardsDealer.innerText = "Dealer's Cards:"
+function UpdateCards() {
+    cardsEl.textContent = "Cards: "
+
+    for (let i = 0; i < cards.length; i++) {
+        if (i > 0) {
+            cardsEl.textContent += ", "
+        }
+        cardsEl.textContent += cards[i]
+    }  
+}
+
+function Hide(elements) {
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].hidden = true
+    }
+}
+
+function Show(elements) {
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].hidden = false
+    }
 }
